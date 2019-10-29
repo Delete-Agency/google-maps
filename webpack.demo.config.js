65b4e821db54a0a4b36c2f67fb0dfd648504d115
anton-github <shockfront00@gmail.com>
2019-10-29 16:15:58 +0500

    move demo built version to dist folder

--- webpack.demo.config.js
+++ webpack.demo.config.js
@@ -2,6 +2,6 @@ const baseConfig = require('./webpack.base.config');
 
 module.exports = (env, argv) => {
     const config = baseConfig(env, argv);
-    config.output.path = __dirname + '/demo';
+    config.output.path = __dirname + '/demo/dist';
     return config;
 };
