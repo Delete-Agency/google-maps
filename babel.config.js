9e3ee5ec4abab425a7fbbc931d623816bc5a1371
anton-github <shockfront00@gmail.com>
2019-11-08 11:33:24 +0500

    publish without bundling

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,17 @@ module.exports = {
             '@babel/preset-env',
             {
                 useBuiltIns: 'usage',
-                corejs: 2
+                corejs: 2,
+                targets: [
+                    "ie 11",
+                    "last 2 firefox versions",
+                    "last 2 edge versions",
+                    "last 2 chrome versions",
+                    "last 2 safari versions",
+                    "last 2 and_chr versions",
+                    "last 2 ios_saf versions"
+                ]
             }
         ]
-    ],
+    ]
 };
--- webpack.build.config.js
+++
@@ -1,9 +0,0 @@
-const baseConfig = require('./webpack.base.config');
-
-module.exports = (env, argv) => {
-    const config = baseConfig(env, argv);
-    config.output.path = __dirname + '/lib';
-    config.output.filename = 'index.js';
-    config.mode = 'development';
-    return config;
-};
